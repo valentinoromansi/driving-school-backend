2ee198d8ce6f81013bb1e833e8cb50034e7a563d
valentinoromansi <valentinoromansi@gmail.com>
2024-02-10 17:55:01 +0100

    init

---
+++ src/main/java/com/example/drivingschool/DrivingschoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.drivingschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivingschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/drivingschool/DrivingschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.drivingschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
