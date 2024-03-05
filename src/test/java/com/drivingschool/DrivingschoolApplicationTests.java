594fe5be27409a5ef19d259a15ab44cb909b1794
Ja <valentinoromansi@gmail.com>
2024-03-05 03:02:59 +0100

    test propertie smoved to resources dir so they can be properly loaded, spring.database.create-and-fill-tables added

--- src/test/java/com/drivingschool/DrivingschoolApplicationTests.java
+++ src/test/java/com/drivingschool/DrivingschoolApplicationTests.java
@@ -2,6 +2,8 @@ package com.drivingschool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 class DrivingschoolApplicationTests {
